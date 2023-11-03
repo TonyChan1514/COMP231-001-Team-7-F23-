637d09a7aa02d4f4e5fef5236fb7cdb306c1b400
TonyChan1514 <tonychan1514@gmail.com>
2023-11-02 22:58:31 -0400

    Revert "Initial Application Coding"
    This reverts commit ad9f094dfc338a60334e41769fde076e67e0cec6.

--- DBProject_COMP231/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module DBProject_COMP214 {
-	requires javafx.controls;
-	requires java.sql;
-	requires java.desktop;
-	requires javafx.base;
-	requires javafx.graphics;
-	
-	opens application to javafx.graphics, javafx.fxml, javafx.base;
-}
